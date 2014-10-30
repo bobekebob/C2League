58bbb283110c8f9d5aeaa3ace065e037c59b0ef8
Martin Bobak <martin.bobak@gmail.com>
2014-10-30 20:47:04 +0100

    organizations and teams administration
    Signed-off-by: Martin Bobak <martin.bobak@gmail.com>

--- C2LeagueAdmin/src/main/java/sk/insomnia/rowingRace/controller/OrganizationAdministrationController.java
+++ C2LeagueAdmin/src/main/java/sk/insomnia/rowingRace/controller/OrganizationAdministrationController.java
@@ -4,6 +4,8 @@ import javafx.fxml.FXML;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import sk.insomnia.rowingRace.dto.EnumEntityDto;
 import sk.insomnia.rowingRace.dto.TeamDto;
 import sk.insomnia.rowingRace.so.School;
 
@@ -15,11 +17,33 @@ public class OrganizationAdministrationController extends AbstractController {
     @FXML
     private ComboBox<School> cbOrganization;
     @FXML
-    private TableView<TeamDto> tbTeams;
+    private TableView<TeamDto> tvTeams;
+    @FXML
+    private TableView<TeamDto> tvSchools;
     @FXML
     private TableColumn<TeamDto, String> tcTeamName;
     @FXML
     private TableColumn<TeamDto, String> tcTeamCategory;
+    @FXML
+    private TableColumn<School, String> tcOrganizationName;
+    @FXML
+    private TableColumn<School, String> tcOrganizationKey;
+    @FXML
+    private TextField tfOrganizationName;
+    @FXML
+    private TextField tfCity;
+    @FXML
+    private TextField tfStreet;
+    @FXML
+    private TextField tfZipCode;
+    @FXML
+    private ComboBox<EnumEntityDto> cbCountry;
+    @FXML
+    private ComboBox<EnumEntityDto> cbRaceCategory;
+    @FXML
+    private TextField tfTeamName;
+    @FXML
+    private ComboBox<EnumEntityDto> cbDisciplineCategory;
 
     @Override
     public void initializeFormData() {
@@ -27,12 +51,30 @@ public class OrganizationAdministrationController extends AbstractController {
     }
 
     @FXML
-    private void deleteOrganization(){
+    private TeamDto newTeam(){
+        return new TeamDto();
+    }
+    @FXML
+    private TeamDto submitTeam(){
+        return null;
+    }
+    @FXML
+    private School newOrganiation() {
+        return new School();
+    }
+
+    @FXML
+    private School submitOrganization() {
+        return null;
+    }
+
+    @FXML
+    private void deleteOrganization() {
 
     }
 
     @FXML
-    private void deleteTeam(){
+    private void deleteTeam() {
 
     }
 }
